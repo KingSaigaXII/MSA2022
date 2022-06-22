1e6a7dad3093dac910b4256ccb6c3f40027495ab
KingSaigaXII <90466258+KingSaigaXII@users.noreply.github.com>
2022-06-22 11:34:45 -0500

    Update stats_calculator.py

--- stats_calculator.py
+++ stats_calculator.py
@@ -32,9 +32,54 @@ def calculate_median(list_of_numbers):
         index_2 = index_1 -1
         median = (list_of_numbers [index_1] + list_of_numbers[index_2]) / 2
     else:
-        median = list_of_numbers[index]
         index = int((len(list_of_numbers) - 1) / 2)
+        median = list_of_numbers[index] 
     return median
+
+def determine_mode(list_of_numbers):
+    modal_values = {}
+    list_of_modal_values = []
+    for number in list_of_numbers:
+        if number in modal_values:
+            modal_values[number] += 1
+        else:
+            modal_values[number] = 1
+    list_values = modal_values.values()
+    max_value = max(list_values)
+    for key in modal_values:
+        if modal_values[key] == max_value:
+            list_of_modal_values.append(key)
+    return list_of_modal_values
+    
+def determine_maximum(list_of_numbers):
+    maximum = list_of_numbers[1]
+    for number in list_of_numbers:
+        if number > maximum:
+            maximum = number
+    return maximum
+
+def determine_minimum(list_of_numbers):
+    minimum = list_of_numbers[1]
+    for number in list_of_numbers:
+        if number < minimum:
+            minimum = number
+    return minimum
+
+def determine_sum(list_of_numbers):
+    sum = 0
+    for number in list_of_numbers:
+        sum = sum + number
+    return sum
+
+def prompt_rerun():
+    user_input = input("Would you like to run this program for another file? (Y/N) ")
+    if user_input == "Y":
+        main()
+    elif user_input == "N":
+        print("Adios")
+    else:
+        print("Incorrect input. Please try again. \n")
+        prompt_rerun()
 
 def main():
     #ask user to enter filename and open that file
@@ -45,8 +90,23 @@ def main():
     elif len(list_of_numbers) == 1:
         print("Only one number in file.")
     else:  
+        maximum = determine_maximum(list_of_numbers)
+        minimum = determine_minimum(list_of_numbers)
+        sum = determine_sum(list_of_numbers)
+        count = len(list_of_numbers)
+        average = round(sum / count, 3)
+        range = maximum - minimum
         median = calculate_median(list_of_numbers)
-        print(median)
-    #load numbers from file into a list
+        list_of_modal_values = determine_mode(list_of_numbers)
+        print(f"File Name: {file_name}")
+        print(f"Sum: {sum}")
+        print(f"Count: {count}")
+        print(f"Average: {average}")
+        print(f"Maximum: {maximum}")
+        print(f"Minimum: {minimum}")
+        print(f"Range: {range}")
+        print(f"Median: {median}")
+        print(f"Mode: {list_of_modal_values}")
+        prompt_rerun()
     
 main()
